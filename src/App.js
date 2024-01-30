4bf9b652a234038dc91724a1fd64886c2c701007
musky <mohamedmusky123@gmail.com>
2024-01-30 13:18:36 +0530

    request body  with alert box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-//import Home from './Pages/Home/Home';
+/* //import Home from './Pages/Home/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 //import UserReq from './Pages/UserRequestHome/UserReq';
 //import Dumydata from './Pages/UserProfile/Dumydata';
 import User_Login from './Pages/Login/User_login';
 import Staff_Login from './Pages/Login/Staff_Login';
 import Forgot from './Pages/Password/Forgot_Pwd';
-import Change from './Pages/Password/Change_Pwd';
-
+import Change from './Pages/Password/Change_Pwd'; */
+import Request from './Pages/Request/Request';
 function App() {
   return (
-    <div className="App">
+    <div>
+   <Request/>
+   </div>
+   /*  <div className="App">
     <BrowserRouter>
     <Routes>
       <Route path='/' element={<User_Login/>} />
@@ -19,7 +22,7 @@ function App() {
       <Route path='/Change' element={<Change/>} />
     </Routes>
     </BrowserRouter>
-    </div>
+    </div> */
   );
 }
 
