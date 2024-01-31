52708d3bf9deb5f2f4cd52e88c55c075b85be5c9
dinukrana2000 <dinuk2000rana@gmail.com>
2024-01-31 09:54:37 +0530

    sign up, complain, vitit pages done by nayanathara.UOR

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 
-/* import Search from './Pages/Historysearch/staffpagehistory-searchday';
-import Activity from './Pages/staffpagehistory-activity-view/Activity';
-import Dailyactivity from './Components/Pages/Dailyactivity/Dailyactivity';
-import Request from './Pages/Request/Request'; */
+ //import Search from './Pages/Historysearch/staffpagehistory-searchday';
+//import Activity from './Pages/staffpagehistory-activity-view/Activity';
+//import Dailyactivity from './Components/Pages/Dailyactivity/Dailyactivity';
+//import Request from './Pages/Request/Request'; 
 
  //import Home from './Pages/Home/Home';
-import { BrowserRouter , Route, Routes } from 'react-router-dom';
+//import { BrowserRouter , Route, Routes } from 'react-router-dom';
 //import UserReq from './Pages/UserRequestHome/UserReq';
 //import Dumydata from './Pages/UserProfile/Dumydata';
-import User_Login from './Pages/Login/User_login';
-import Staff_Login from './Pages/Login/Staff_Login';
-import Forgot from './Pages/Password/Forgot_Pwd';
-import Change from './Pages/Password/Change_Pwd'; 
+//import User_Login from './Pages/Login/User_login';
+//import Staff_Login from './Pages/Login/Staff_Login';
+//import Forgot from './Pages/Password/Forgot_Pwd';
+//import Change from './Pages/Password/Change_Pwd'; 
+import StaffComplain from './Pages/staffcomplain/staffcomplain';
 
 
 function App() {
@@ -28,14 +29,7 @@ function App() {
    </div>  */
 
     <div className="App">
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<User_Login/>} />
-      <Route path='/Staff_Login' element={<Staff_Login/>} />
-      <Route path='/Forgot' element={<Forgot/>} />
-      <Route path='/Change' element={<Change/>} />
-    </Routes>
-    </BrowserRouter>
+    <StaffComplain/>
     </div> 
   );
 }
