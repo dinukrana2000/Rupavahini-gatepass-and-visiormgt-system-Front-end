33d36e24806a347a80ee6612d856c7c1738b575a
GayaliKaveesha <gayalinanayakkara@gmail.com>
2024-03-03 03:34:46 +0530

    did some changes

--- src/Pages/Sec_Visit/Visit_Rup.jsx
+++ src/Pages/Sec_Visit/Visit_Rup.jsx
@@ -5,15 +5,17 @@ import MuiAccordionSummary from '@mui/material/AccordionSummary';
 import MuiAccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import Drawer from '../../Components/Drawer/Drawer';
-import React, { useState } from 'react';
-import { Button, Grid, Select, TextField, InputLabel, MenuItem, FormControl } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Button, Grid, Select, TextField, InputLabel, MenuItem, FormControl, Box } from '@mui/material';
+import axios from 'axios';
 
 const BackgroundImg = styled('div') ({
     backgroundImage: 'url("https://assets-eu-01.kc-usercontent.com/77bbf83a-1306-0152-fea5-3b5eaf937634/7916661a-40c3-4d65-8ea8-6785dfe41ab8/GettyImages-1303567646.jpg")',
     backgroundSize: 'cover',
     display: 'flex',
     backgroundAttachment: "fixed",
-    height: '100vh',
+    minHeight: '100vh',
+    height: 'auto',
     overflow: 'hidden',
     justifyContent: 'center',
     fontFamily: '"Roboto", sans-serif'
@@ -73,36 +75,90 @@ const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
 }));
 
 function Visit_Rup() {
-  const [expanded, setExpanded] = useState(false);
+    const [expanded, setExpanded] = useState(false);
   const [showAdditionalTypography, setShowAdditionalTypography] = useState(false);
-
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
-
   const [In_Out, setIn_Out] = useState('');
   const [timeValue, setTimeValue] = useState('');
 
-  const handleChangeInOut = (event) => {
-    setIn_Out(event.target.value);
-  };
-
-  const handleChangeTime = (event) => {
-    setTimeValue(event.target.value);
-  };
-
-  const handleEnterClick = () => {
-    setTimeValue('');
-    setIn_Out('');
-
-    console.log('Time: ', timeValue);
-    console.log('In/Out: ', In_Out)
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
   };
 
   const handleButtonClick = () => {
     setShowAdditionalTypography(!showAdditionalTypography);
   };
 
+  const handleChangeTime = (event) => {
+    setTimeValue(event.target.value);
+  };
+
+  const handleChangeInOut = (event) => {
+    setIn_Out(event.target.value);
+  };
+
+  const handleEnterClick = () => {
+    // const updatedReportData = reportData.map(item => {
+    //   if (item.label === "Time Out") {
+    //     return { ...item, value: In_Out === "out" ? timeValue : item.value }; // Update time out only if In_Out is 'Out'
+    //   } else if (item.label === "Time in") {
+    //     return { ...item, value: In_Out === 'in' ? timeValue : item.value }; // Update time in only if In_Out is 'In'
+    //   }
+    //   return item;
+    // // });
+  
+    // setReportData(updatedReportData);
+
+    setTimeValue('');
+    setIn_Out('');
+
+    console.log('Time: ', timeValue);
+    console.log('In/Out: ', In_Out);
+  };
+  
+  const [sclbookings, setSclBookings] = useState([]);
+  const [unibookings, setUniBookings] = useState([]);
+  const [loading, setLoading] = useState(true);
+  // const [userData, setUserData] = useState(null);
+  // const [useID, setUserID] = useState("");
+  const [userRole, setUserRole] = useState("");
+
+  useEffect(() => {
+    // fetchUserData();
+    fetchBookings();
+  }, [userRole]);
+
+  // const fetchUserData = async () => {
+  //   const data = await getUserData();
+  //   console.log(data);
+
+  //   if (data) {
+  //     setUserData(data);
+  //     setUserID(data.id);
+  //     setUserRole(data.userType);
+  //     console.log(userRole);
+  //   }
+  // };
+
+  const fetchBookings = async () => {
+    if (userRole === 'security') {
+      await axios 
+      .get("")
+      .then((response) => {
+        console.log(response.data);
+        setSclBookings(response.data);
+        setLoading(false);
+      })
+      .get("")
+      .then((response) => {
+        console.log(response.data);
+        setUniBookings(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching booking data",error);
+      });
+    }
+  };
 
   return (
     <>
@@ -110,18 +166,66 @@ function Visit_Rup() {
     <BackgroundImg>
       <Container>
         <h2><u>Visit Rupavahini</u></h2>
-        
-        <Grid width={"100%"} >
-              <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-              <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
-                <Typography><b>Royal College</b></Typography>
-              </AccordionSummary>
-              <AccordionDetails>
+        <h3>School</h3>
+          <Grid width={"100%"} >
+            {loading ? (
+              <Box style={{width:'100%', background:'#D6C9CA', padding:'.5rem'}}>
+                <Typography>Loading...</Typography>
+              </Box>
+            ) : (
+              sclbookings.map((booking, index) => (
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
+                  <Typography><b>{booking.shoolname}</b></Typography>
+                </AccordionSummary>
+                <AccordionDetails>
                   <Typography>
-                    School Name : <br/>
-                    Grade: <br/>
-                    No of Students: <br/>
-                    Address: <br/>
+                    <table>
+                      <tr>
+                        <td>School Name:</td>
+                        <td> {booking.shoolname} </td>
+                      </tr>
+                      <tr>
+                        <td>Grade:</td>
+                        <td>{booking.grade}</td>
+                      </tr>
+                      <tr>
+                        <td>Authorized Person's Name:</td>
+                        <td>{booking.Rname}</td>
+                      </tr>
+                      <tr>
+                        <td>Phone No:</td>
+                        <td>{booking.cnumber}</td>
+                      </tr>
+                      <tr>
+                        <td>No of students:</td>
+                        <td>{booking.Mstudent}</td>
+                        {/* <td>{booking.Mstudent}+{booking.Fstudent}</td> */}
+                      </tr>
+                      <tr>
+                        <td>No of teachers:</td>
+                        <td>{booking.noteachers}</td>
+                      </tr>
+                      <tr>
+                        <td>No of parents:</td>
+                        <td>{booking.noparents}</td>
+                      </tr>
+		                  <tr>
+                        <td>Time In:</td>
+                        <td>{booking.timeIN}</td>
+                      </tr>
+		                  <tr>
+                        <td>Time Out:</td>
+                        <td>{booking.timeOUT}</td>
+                      </tr>
+                    </table>
+                        {/* {reportData.map((item, index) => (
+                          <div key={index} >
+                            <span>{item.label}:</span>
+                            <span>{item.label === 'Time Out' || item.label === 'Time in' ? item.value : '-'}</span>
+                          </div>
+                        ))} */}
+
                   </Typography>
                   <hr/>
                   <Grid container justifyContent="center" paddingBottom={'2%'}>
@@ -146,8 +250,8 @@ function Visit_Rup() {
                                 label="In/Out"
                                 onChange={handleChangeInOut} >
 
-                                  <MenuItem value={10}>In</MenuItem>
-                                  <MenuItem value={20}>Out</MenuItem>
+                                  <MenuItem value={"in"}>In</MenuItem>
+                                  <MenuItem value={"out"}>Out</MenuItem>
 
                               </Select>
                             </FormControl>
@@ -160,22 +264,64 @@ function Visit_Rup() {
                     </Grid> 
                   )}
 
-              </AccordionDetails>
-           </Accordion>
-      
+                </AccordionDetails>
+                </Accordion>
+              ))
+            )}
           </Grid>
-
+                  
+          <h3>University</h3>
           <Grid width={"100%"} >
-              <Accordion expanded={expanded === 'panel12'} onChange={handleChange('panel12')}>
-              <AccordionSummary aria-controls='panel12d-content' id='panel12d-header'>
-                <Typography><b>Royal College</b></Typography>
-              </AccordionSummary>
-              <AccordionDetails>
+          {loading ? (
+              <Box style={{width:'100%', background:'#D6C9CA', padding:'.5rem'}}>
+                <Typography>Loading...</Typography>
+              </Box>
+            ) : (
+              unibookings.map((booking, index) => (
+                <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                <AccordionSummary aria-controls='panel1d-content' id='panel1d-header'>
+                  <Typography><b>{booking.uniname}</b></Typography>
+                </AccordionSummary>
+                <AccordionDetails>
                   <Typography>
-                    School Name : <br/>
-                    Grade: <br/>
-                    No of Students: <br/>
-                    Address: <br/>
+                    <table>
+                      <tr>
+                        <td>University/Coolege Name:</td>
+                        <td> {booking.uniname} </td>
+                      </tr>
+                      <tr>
+                        <td>Authorized Person's Name:</td>
+                        <td>{booking.Rname}</td>
+                      </tr>
+                      <tr>
+                        <td>Phone No:</td>
+                        <td>{booking.cnumber}</td>
+                      </tr>
+                      <tr>
+                        <td>No of students:</td>
+                        <td>{booking.Mstudent}</td>
+                        {/* <td>{booking.Mstudent}+{booking.Fstudent}</td> */}
+                      </tr>
+                      <tr>
+                        <td>No of other visitors:</td>
+                        <td>{booking.noparents}</td>
+                      </tr>
+		                  <tr>
+                        <td>Time In:</td>
+                        <td>{booking.timeIN}</td>
+                      </tr>
+		                  <tr>
+                        <td>Time Out:</td>
+                        <td>{booking.timeOUT}</td>
+                      </tr>
+                    </table>
+                        {/* {reportData.map((item, index) => (
+                          <div key={index} >
+                            <span>{item.label}:</span>
+                            <span>{item.label === 'Time Out' || item.label === 'Time in' ? item.value : '-'}</span>
+                          </div>
+                        ))} */}
+
                   </Typography>
                   <hr/>
                   <Grid container justifyContent="center" paddingBottom={'2%'}>
@@ -186,12 +332,11 @@ function Visit_Rup() {
 
                     <Grid backgroundColor={'#D6C9CA'} padding={'2%'}>
 
-                    <Typography>
                     <Grid container spacing={1}>
-                      <Grid item xs={12} sm={4}>
-                        <TextField id='time' label='Time' type='time' variant='standard' size='small' focused fullWidth/>
+                      <Grid item xs={12} sm={4} justifyContent={'center'}>
+                        <TextField id='time' label='Time' type='time' variant='standard' size='small'focused value={timeValue} onChange={handleChangeTime}/>
                       </Grid>
-                      <Grid item xs={12} sm={4}>
+                      <Grid item xs={12} sm={3}>
                             <FormControl size="small" fullWidth>
                               <InputLabel id="demo-simple-select-label">In/Out</InputLabel>
                               <Select
@@ -201,25 +346,24 @@ function Visit_Rup() {
                                 label="In/Out"
                                 onChange={handleChangeInOut} >
 
-                                  <MenuItem value={10}>In</MenuItem>
-                                  <MenuItem value={20}>Out</MenuItem>
+                                  <MenuItem value={"in"}>In</MenuItem>
+                                  <MenuItem value={"out"}>Out</MenuItem>
 
                               </Select>
                             </FormControl>
                       </Grid>
-                      <Grid item xs={12} sm={4}>
-                        <Button variant='outlined'  >Enter</Button>
+                      <Grid item xs={12} sm={4} style={{ textAlign: 'right' }}>
+                        <Button variant='outlined' onClick={handleEnterClick} >Enter</Button>
                       </Grid>
                     </Grid>
-                      
-                    </Typography>
-
+                    
                     </Grid> 
                   )}
 
-              </AccordionDetails>
-           </Accordion>
-      
+                </AccordionDetails>
+                </Accordion>
+              ))
+            )}
           </Grid>
 
       </Container>
